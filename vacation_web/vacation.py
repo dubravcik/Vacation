63a6b8c79cfbeb92173c5b633fee909c17d47c57
Michal Dubravcik <michal.dubravcik@avg.com>
2016-04-17 11:35:24 +0200

    web working

---
+++ vacation_web/vacation.py
@@ -0,0 +1,45 @@
+__author__ = 'michal.dubravcik'
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+
+from contextlib import closing
+
+# configuration
+DATABASE = '../vacation.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_instance_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
+@app.route('/')
+def show_entries():
+    cur = g.db.execute('SELECT title,text FROM entries ORDER BY id DESC')
+    entries = [dict(title=row[0],text=row[1]) for row in cur.fetchall()]
+    return render_template('show_entries.html', entries=entries)
+
+
+
+if __name__ == '__main__':
+    app.run(port=80)
